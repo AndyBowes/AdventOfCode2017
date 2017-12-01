943b3d1dc3f2639df8d679f400caeb20ceaf3e31
Andy Bowes <48afe687@opayq.com>
2017-12-01 20:38:40 +0000

    Solution of Day 1

---
+++ src/main/kotlin/jfactory/aoc2017/day1/NaughtyOrNice.kt
@@ -0,0 +1,22 @@
+package jfactory.aoc2017.day1
+
+
+
+fun naughtyOrNice(input: String): Int {
+    val nums = input.map { it.charToInt() }
+    return nums.zip(listOf(nums.last()).plus(nums))
+            .filter { it.first == it.second }
+            .map{it.first}.sum()
+}
+
+fun Char.charToInt() = if (this.isDigit()) this.toString().toInt() else throw IllegalArgumentException("Not a digit :$this")
+
+fun naughtyOrNice2(input: String): Int {
+    val nums = input.map { it.charToInt() }
+    val nums2 = (input.substring(input.length/2) + input).map { it.charToInt() }
+    return nums.zip(nums2)
+            .filter { it.first == it.second }
+            .map{it.first}.sum()
+}
+
+
