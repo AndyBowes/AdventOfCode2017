5fbc46c67aa7aa5e2c7061fccc2a6b16665a570a
Andy Bowes <48afe687@opayq.com>
2017-12-15 15:35:28 +0000

    Tidy up Day 15

--- src/main/kotlin/jfactory/aoc2017/day15/Generators.kt
+++ src/main/kotlin/jfactory/aoc2017/day15/Generators.kt
@@ -1,19 +1,23 @@
 package jfactory.aoc2017.day15
 
-fun Long.lowestBits(n:Int) = this.and( Math.pow(2.toDouble(), n.toDouble()).toLong() - 1)
-fun Int.lowestBits(n:Int) = this.and( Math.pow(2.toDouble(), n.toDouble()).toInt() - 1)
+//fun Long.lowestBits(n: Int) = this.and(Math.pow(2.toDouble(), n.toDouble()).toLong() - 1)
+//fun Int.lowestBits(n: Int) = this.and(Math.pow(2.toDouble(), n.toDouble()).toInt() - 1)
+//
+
+fun Long.lowBits() = this and 0xffff
+private fun numGenerator(seed: Long, multiplier: Int) = generateSequence(seed) { (it * multiplier) % 2147483647 }
 
 fun main(args: Array<String>) {
 
-    val genA = generateSequence(634L){ ((it*16807) % 2147483647)}
-    val genB = generateSequence(301L){ ((it*48271) % 2147483647)}
+    val genA = numGenerator(634L, 16807)
+    val genB = numGenerator(301L, 48271)
 
-    println( genA.zip(genB)
+    println(genA.zip(genB)
             .take(40_000_000)
-            .count{it.first.and(65535) == it.second.and(65535)})
+            .count { (a, b) -> a and 0xffff == b and 0xffff })
 
-    println( genA.filter { it % 4L == 0L }.zip(genB.filter { it % 8L == 0L })
+    println(genA.filter { it % 4L == 0L }.zip(genB.filter { it % 8L == 0L })
             .take(5_000_000)
-            .count{it.first.and(65535) == it.second.and(65535)})
+            .count { (a, b) -> a.lowBits() == b.lowBits() })
 
 }
