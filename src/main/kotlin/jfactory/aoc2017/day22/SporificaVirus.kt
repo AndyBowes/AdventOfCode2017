ffa847db21e27c30c9acec9d5b90b2632fe8aacc
Andy Bowes <48afe687@opayq.com>
2017-12-22 18:51:00 +0000

    Solution for Day 22 - Part 2

--- src/main/kotlin/jfactory/aoc2017/day22/SporificaVirus.kt
+++ src/main/kotlin/jfactory/aoc2017/day22/SporificaVirus.kt
@@ -1,7 +1,9 @@
 package jfactory.aoc2017.day22
 
+import kotlin.system.measureTimeMillis
+
 class Grid(initalState: String, val debug:Boolean = false){
-    val map = mutableMapOf<Pair<Int,Int>,Char>()
+    val map = mutableMapOf<String,Char>()
     var direction = Pair(0,-1)
 
     var currentPos = Pair(0,0)
@@ -14,9 +16,9 @@ class Grid(initalState: String, val debug:Boolean = false){
     }
 
     fun charAt(pos: Pair<Int,Int>) = charAt(pos.first,pos.second)
-    fun charAt(x:Int,y:Int) = map.getOrPut(x to y, {'.'})
+    fun charAt(x:Int,y:Int) = map.getOrPut("$x&$y", {'.'})
     fun setChar(pos:Pair<Int,Int>, c:Char) = setChar(pos.first,pos.second, c)
-    fun setChar(x:Int, y:Int, c: Char) = map.put(x to y, c)
+    fun setChar(x:Int, y:Int, c: Char) = map.put("$x&$y", c)
 
     fun turnLeft(){
         direction = Pair(direction.second, direction.first * -1)
@@ -32,8 +34,10 @@ class Grid(initalState: String, val debug:Boolean = false){
 
     fun move(){
         when (charAt(currentPos)){
-            '#' -> cleanNode(currentPos)
-            else -> infectNode(currentPos)
+            'W' -> infectNode(currentPos)
+            'F' -> cleanNode(currentPos)
+            '#' -> flagNode(currentPos)
+            else -> weakenNode(currentPos)
         }
         currentPos = Pair(currentPos.first + direction.first, currentPos.second + direction.second)
 
@@ -48,13 +52,22 @@ class Grid(initalState: String, val debug:Boolean = false){
     }
 
     private fun cleanNode(pos: Pair<Int, Int>) {
-        turnRight()
+        reverse()
         setChar(pos, '.')
+    }
+
+    private fun weakenNode(pos: Pair<Int, Int>){
+        turnLeft()
+        setChar(pos, 'W')
+    }
+
+    private fun flagNode(pos: Pair<Int, Int>){
+        turnRight()
+        setChar(pos, 'F')
     }
 
     private fun infectNode(pos: Pair<Int,Int>){
         infected += 1
-        turnLeft()
         setChar(pos, '#')
     }
 
@@ -67,7 +80,7 @@ class Grid(initalState: String, val debug:Boolean = false){
 
 fun main(args: Array<String>) {
     val grid = Grid(input)
-    println(grid.doMoves(10000))
+    println(grid.doMoves(10_000_000))
 }
 
 
